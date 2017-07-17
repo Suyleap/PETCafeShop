5545fb45f45b2afd3de14e266f2f794e1513ee99
Sorindy <hot.sorindy@gmail.com>
2017-07-17 21:32:52 +0700

    database cannot edit

--- PetCafeShop/PetCafeShop/StaffForm.vb
+++ PetCafeShop/PetCafeShop/StaffForm.vb
@@ -43,10 +43,13 @@
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         st.InsertStaff(txtID.Text, txtName.Text, cboGender.Text, Convert.ToDateTime(dtpDOB.Text), txtAddress.Text, txtPhone.Text, txtEmail.Text, txtPosition.Text, Convert.ToDouble(txtSalary.Text), Convert.ToDateTime(dtpSWD.Text))
-        dgvStaff.DataSource = st.Show()
+        reload()
+        'Clear()
         Me.Refresh()
     End Sub
-
+    Private Sub reload()
+        dgvStaff.DataSource = st.Show()
+    End Sub
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
 
         st.DeleteStaff(dgvStaff.CurrentRow.Cells(0).Value.ToString())
